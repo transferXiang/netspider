90da1a48360f8bc34bd4a00e544507da464008b7
transfer <448217518@qq.com>
2017-02-18 00:27:13 +0800

    提交获取工作信息的基本功能，后续继续进行优化

--- jobinfo.py
+++ jobinfo.py
@@ -4,6 +4,7 @@ import requests
 import os
 import sys
 from bs4 import BeautifulSoup
+import jieba.posseg as fenci
 
 
 def _string_list_save(save_path, filename, slist):
@@ -12,17 +13,19 @@ def _string_list_save(save_path, filename, slist):
     path = save_path + "/" + filename + ".txt"
     with open(path, "w+") as fp:
         for s in slist:
-            fp.write("%s\t\t%s\n" % (s['rank'].encode("utf8"), s['name'].encode("utf8")))
+            fp.write("%s\t\t%s\n" % (s['title'].encode("utf8"), s['url'].encode("utf8")))
 
 
 def _get_page_info(soup):
     my_page_info = []
-    for tag in soup.find_all('div', class_='style2 stylebg'):
-        print tag.span.a.getText()
-        my_page_info.append(tag.span.a.getText())
-    for tag in soup.find_all('div', class_='style2'):
-        print tag.span.a.getText()
-        my_page_info.append(tag.span.a.getText())
+    class_types = ['style2 stylebg','style2']
+    for class_type in class_types:
+        for tag in soup.find_all('div', class_=class_type):
+            title = tag.span.a.getText()
+            print title
+            url = tag.span.a['href']
+            item = {'title':title, 'url':url}
+            my_page_info.append(item)
     return my_page_info
 
 
@@ -52,7 +55,20 @@ def _get_movie_list(prefix_url, suffix_url):
 
 def spider(url, save_path, filename):
     movie_info_list = _get_movie_list(url, "")
-    _string_list_save(save_path, filename, movie_info_list)
+    save_infos = []
+    key_words = [u'生物', u'生命科学', u'生物医学工程', u'生物医学光子学', u'光学']
+    for item in movie_info_list:
+        print 'search', item['title']
+        job_detail = requests.get(item['url']).content
+        soup = BeautifulSoup(job_detail, "html.parser", from_encoding='gb18030')
+        context = soup.find('div', class_='article_body').getText()
+        words = fenci.cut(context)
+        for w in words:
+            if w.word in key_words:
+                save_infos.append(item)
+                break
+
+    _string_list_save(save_path, filename, save_infos)
 
 
 if __name__ == '__main__':
