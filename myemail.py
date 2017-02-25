d4566b25acb31f5dc590e613daaec08cc1c93abf
transfer <448217518@qq.com>
2017-02-25 21:25:19 +0800

    支持附件的发送

--- myemail.py
+++ myemail.py
@@ -2,7 +2,10 @@
 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 import ConfigParser
+from os import path
 
 
 
@@ -15,17 +18,40 @@ class MyEmail:
     def __del__(self):
         self.host.quit()
 
-    def send(self, to, title, context):
-        msg = MIMEText(context)
-        msg["From"] = self.user_name
-        msg["To"] = to
-        msg["Subject"] = title
-
+    def _sendemail(self, user_name, to, msg):
         try:
-            self.host.sendmail(self.user_name, to, msg.as_string())
+            self.host.sendmail(user_name, to, msg.as_string())
             print("send email success to %s" % to)
         except smtplib.SMTPException, e:
             print "Falied,%s" % e
+
+    def send_text(self, to, title, context):
+        msg = MIMEText(context)
+        msg["Subject"] = title
+        msg["From"] = self.user_name
+        msg["To"] = to
+        self._sendemail(self.user_name, to, msg)
+
+    def send_files(self, to, title, context, file_paths):
+        msg = MIMEMultipart()
+        msg["Subject"] = title
+        msg["From"] = self.user_name
+        msg["To"] = to
+
+        # 文字部分(邮件主体)
+        text_part = MIMEText(context)
+        msg.attach(text_part)
+
+        # 附件部分
+        for file_path in file_paths:
+            with open(file_path, 'rb') as fp:
+                attachment = fp.read()
+                part = MIMEApplication(attachment)
+                file_name = path.basename(file_path)
+                part.add_header('Content-Disposition', 'attachment', filename=file_name)
+                msg.attach(part)
+
+        self._sendemail(self.user_name, to, msg)
 
 
 if __name__ == '__main__':
@@ -37,5 +63,12 @@ if __name__ == '__main__':
     port = config.getint('host', 'port')
 
     email = MyEmail(user_name, pwd, host_name, port)
-    email.send('448217518@qq.com', 'this is a test email', 'hellow world')
+
+    # 发送普通邮件
+    to = '448217518@qq.com'
+    email.send_text(to, 'text email test', 'hellow world')
+
+    # 发送附件
+    file_paths = ['./myemail.py', './mymongodb.py']
+    email.send_files(to, 'attachment email test', '请看附件', file_paths)
 
