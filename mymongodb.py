e6bf944cc60acede44b4dcb69d6c6944b888bae2
transferXiang <448217518@qq.com>
2017-03-30 01:10:53 +0800

    修改自定义mongodb，使得其能够在python3上运行

--- mymongodb.py
+++ mymongodb.py
@@ -6,10 +6,10 @@ import random
 
 
 class MyMongoDb:
-    def __init__(self, db_name, cellection_name, host="localhost", port=27017):
+    def __init__(self, db_name, collection_name, host="localhost", port=27017):
         client = pymongo.MongoClient(host, port)
         self.db = client.get_database(db_name)
-        self.collection = self.db.get_collection(cellection_name)
+        self.collection = self.db.get_collection(collection_name)
 
     def insert(self, data):
         self.collection.insert(data)
@@ -23,20 +23,19 @@ class MyMongoDb:
     def find(self,):
         return self.collection.find()
 
-    def find_one_and_update(self, filter, update):
-        self.collection.find_one_and_update(filter, {'$set': update})
+    def find_one_and_update(self, my_filter, update):
+        self.collection.find_one_and_update(my_filter, {'$set': update})
 
 
 if __name__ == "__main__":
     db = MyMongoDb("Test", "myCollection")
     db.drop_collection("myCollection")
-    db.insert([{"time": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))},
-               {"val": random.random()}])
-    if db.contians({"time": time.time()}):
-        print "find"
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    db.insert([{"time": time_str}, {"val": random.random()}])
+    if db.contians({"time": time_str}):
+        print("find")
     else:
-        print "not find"
+        print("not find")
 
     for doc in db.collection.find():
-        if doc.has_key('time'):
-            print doc['time']
+        print(doc)
