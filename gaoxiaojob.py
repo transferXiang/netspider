8748d8e27683bcb9902aa71662685a7491ca874b
transferXiang <448217518@qq.com>
2017-04-02 00:24:52 +0800

    fix for python 3.x

--- gaoxiaojob.py
+++ gaoxiaojob.py
@@ -6,38 +6,67 @@ import jieba.posseg as fenci
 
 
 class GaoxiaoJobInfoUrlCollector(AbsUlrCollector):
+    '''
+    网页信息获取对象
+    '''
     def __init__(self, db_name, collection_name):
         self.db = MyMongoDb(db_name, collection_name)
 
     def get_next_page_suffix(self, soup):
+        '''
+        获取“下一页”的后缀，用于递归获取网页信息
+        :param soup: soup对象
+        :return: 下一页网页后缀
+        '''
         suffix_url = ''
         next_page_tag = soup.find_all('a', class_='next')
-        if len(next_page_tag) == 1:
+        if next_page_tag:
             suffix_url = next_page_tag[0]['href']
         return suffix_url
 
     def parse_page_info(self, soup):
+        '''
+        获取当前界面中的url信息
+        :param soup: soup对象
+        :return: 当前界面中url信息list
+        '''
         page_info = []
         class_types = ['style2 stylebg', 'style2']
         for class_type in class_types:
             for tag in soup.find_all('div', class_=class_type):
                 title = tag.span.a.getText()
                 url = tag.span.a['href']
-                item = {'title': title, 'url': url, 'processed': False}
-                page_info.append(item)
+                # 第一次获取到的url，所以还未进行处理
+                url_item = {'title': title, 'url': url, 'processed': False}
+                page_info.append(url_item)
         return page_info
 
     def save_page_info(self, info):
+        '''
+        保存url信息
+        :param info: url信息
+        :return: none
+        '''
         for info_item in info:
-            if not self.db.contians({'url': info_item['url']}):
-                print 'insert:' + info_item['title']
-                self.db.insert(info_item)
+            # 如果已经存在则不再保存
+            if self.db.contians({'url': info_item['url']}):
+                continue
+            # 保存
+            print('insert:{title}'.format(title=info_item['title']))
+            self.db.insert(info_item)
 
     def get_urls(self):
+        '''
+        获取还未分析过的url
+        :return: url list
+        '''
         return self.db.collection.find({'processed': False})
 
 
 class GaoxiaoContexParse(AbsContexParse):
+    '''
+    根据网页信息进行解析，找到感兴趣的网页
+    '''
     def __init__(self, db_name, collection_name, include_key_words):
         self.db = MyMongoDb(db_name, collection_name)
         self.include_key_words = include_key_words
@@ -46,6 +75,7 @@ class GaoxiaoContexParse(AbsContexParse):
         return soup.find('div', class_='article_body').getText()
 
     def process_context(self, url, context):
+        # 已经分析，则更新数据库
         self.db.find_one_and_update({'url': url}, {'processed': True})
 
         words = fenci.cut(context)
@@ -57,10 +87,9 @@ class GaoxiaoContexParse(AbsContexParse):
                     matched_kes.append(w.word)
 
         if len(matched_kes) > 0:
-            print '\nmatch key',
+            print('\nmatch key ', end="")
             for word in matched_kes:
-                print word,
-            print ''
+                print(word, end="")
             return True
 
         return False
@@ -78,11 +107,11 @@ if __name__ == '__main__':
     parse = GaoxiaoContexParse(db_name, collection_name, key_words)
 
     times = 0
-    for item in jobInfoUrl.get_urls():
+    url_list = jobInfoUrl.get_urls()
+    total = url_list.count()
+    for item in url_list:
         times += 1
-        if times % 100 == 0:
-            print '.'
-        else:
-            print '.',
+        print("{cur}/{total}".format(cur=times, total=total))
         if parse.analysis_page(item['url']):
-            print('find %s\n' % item['title'])
+            print('\tfind %s' % item['title'])
+    print("完成")
--- jobinfo.py
+++ jobinfo.py
@@ -3,43 +3,45 @@
 from gaoxiaojob import GaoxiaoContexParse
 from gaoxiaojob import GaoxiaoJobInfoUrlCollector
 from myemail import MyEmail
-import ConfigParser
+import configparser
 import time
 
 if __name__ == '__main__':
     db_name = 'jbDb'
-    cellection_name = 'info_url'
+    collection_name = 'info_url'
 
-    #  从网站获取需要抓取的url
-    jobInfoUrl = GaoxiaoJobInfoUrlCollector(db_name, cellection_name)
+    # 从网站获取需要抓取的url
+    jobInfoUrl = GaoxiaoJobInfoUrlCollector(db_name, collection_name)
     start_url = "http://www.gaoxiaojob.com/zhaopin/chengshi/shenzhen/"
     jobInfoUrl.get_url_list(start_url)
 
+    # 解析器
     key_words = [u'生物', u'生命科学', u'生物医学工程', u'生物医学光子学']
-    parse = GaoxiaoContexParse(db_name, cellection_name, key_words)
+    parse = GaoxiaoContexParse(db_name, collection_name, key_words)
 
     # 解析有用的网页
     context = ''
     times = 0
-    infos = jobInfoUrl.get_urls()
-    for item in infos:
+    info_list = jobInfoUrl.get_urls()
+    for item in info_list:
         times += 1
-        print times
+        print(times)
 
         if parse.analysis_page(item['url']):
             print('find %s\n' % item['title'])
             context += ('%s\n%s\n\n' % (item['title'], item['url']))
 
     if context != '':
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
+        section_name = 'host'
         config.read("email.ini")
-        user_name = config.get('host', 'user_name')
-        pwd = config.get('host', 'pwd')
-        host_name = config.get('host', 'host_name')
-        port = config.getint('host', 'port')
+        user_name = config.get(section_name, 'user_name')
+        pwd = config.get(section_name, 'pwd')
+        host_name = config.get(section_name, 'host_name')
+        port = config.getint(section_name, 'port')
 
         # 将获取的信息通过邮件发送出去
         email = MyEmail(user_name, pwd, host_name, port)
-        to = '448217518@qq.com'
+        to = config.get(section_name, 'send_to')
         title = 'job info ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         email.send_text(to, title, context)
